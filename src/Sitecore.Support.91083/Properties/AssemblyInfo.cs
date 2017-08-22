a5c4472140d5273141a0500a1beaa5fe5bf134d5
Eugene Oparyshev <eop@sitecore.net>
2017-08-22 11:05:48 +0300

    Github patch

---
+++ src/Sitecore.Support.91083/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.91083")]
+[assembly: AssemblyProduct("Sitecore.Support.91083")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.91083/Syndication/CustomFeed.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Syndication
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Syndication;
+  using System.Collections.Generic;
+  using System.Linq;
+  public class CustomFeed : PublicFeed
+  {
+    // Methods
+    public override IEnumerable<Item> GetSourceItems()
+    {
+      List<Item> list = base.GetSourceItems().ToList<Item>();
+      List<Item> list2 = new List<Item>();
+      foreach (Item item in list)
+      {
+        Item item2 = Context.Database.GetItem(item.ID, Context.Language);
+        if ((item2 == null) || (item2.Versions.Count == 0))
+        {
+          list2.Add(item);
+        }
+      }
+      foreach (Item item in list2)
+      {
+        list.Remove(item);
+      }
+      return list;
+    }
+  }
+
+
+}
